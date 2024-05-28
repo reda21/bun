9d65197108cc03be932f5dec2b0c5e8060c69e59
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-05-28 11:25:11 +0100

    ajout ; dans la linge 3

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { generatePassword } from "./lib/app";
 
-console.info('tester le clone') 
+console.info("tester le clone");
 
 console.info(
   "new password is: " +
     generatePassword({
       length: 30,
-      useSymbols: true
+      useSymbols: true,
     })
 );
