e3fd585c2092d3441482dba6a709b74f2db28153
reda cherfaoui <redacherfaoui@gmail.com>
2024-05-28 22:50:08 +0100

    upsdate server

--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
+import {time} from "./lib/times"
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+// Lire le fichier HTML une seule fois
+const html = readFileSync(join('template', 'index.html'), 'utf-8');
+
 Bun.serve({
   port: 8070, // Utiliser un autre port
- // hostname: "mydomain.me",
+  // hostname: "mydomain.me",
   fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname === "/") return new Response("Home page!");
-    if (url.pathname === "/blog") return new Response("Blog!");
-    return new Response("404!");
+    if (url.pathname === "/") {
+      return new Response(html, {
+        headers: { "Content-Type": "text/html" }
+      });
+    }
+    if (url.pathname === "/time") {
+      return new Response(JSON.stringify({ currentTime: time() }), {
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+    return new Response("404!", { status: 404 });
   },
 });
