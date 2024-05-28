478a93fd97182429bf13ea6fa02c1d6920b7960f
Reda Cherfaoui <redacherfaoui@gmail.com>
2024-05-28 14:55:26 +0100

    valider les changement clone

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 import { generatePassword, hashPassword } from "./lib/app";
+import { logger } from "./lib/logger";
 
 const password = generatePassword({
   length: 30,
   useSymbols: true,
 });
 
-console.info(`le nouveau mot passe est: ${password}, son hashage est de ${hashPassword(password)}`);
+logger.info(
+  `le nouveau mot passe est: ${password}, son hashage est de ${hashPassword(
+    password
+  )}`
+);
 
-console.info("done");
+logger.info("done");
